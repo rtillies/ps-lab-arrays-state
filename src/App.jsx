4821b0ec0faadad9cafad3a6ff0dcd865bcabd5d
Richard T <74796181+rtillies@users.noreply.github.com>
2024-03-21 15:57:44 -0400

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import Learner from './components/Learner/Learner'
-// import './App.css'
-// import { initialLearners } from './data'
 
 function App() {
-  // const [learners, setLearners] = useState(initialLearners)
 
   return (
     <>
